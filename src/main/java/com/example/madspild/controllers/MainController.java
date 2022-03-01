0f86e0d1373aa3cb69dc4406d41e37c4bf3caea1
Eliot996 <math10t6@stud.kea.dk>
2022-03-01 14:14:28 +0100

    added links to the recipes

--- src/main/java/com/example/madspild/controllers/MainController.java
+++ src/main/java/com/example/madspild/controllers/MainController.java
@@ -4,6 +4,7 @@ import com.example.madspild.repository.Recipes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -19,5 +20,11 @@ public class MainController {
     public String allRecipes(Model model) {
         model.addAttribute("recipes", RECIPES.getAllRecipes());
         return "Recipes";
+    }
+
+    @GetMapping("/opskrifter/{name}")
+    public String allRecipes(Model model, @PathVariable() String name) {
+        model.addAttribute("recipe", RECIPES.getRecipe(name));
+        return "Recipe";
     }
 }
--- src/main/java/com/example/madspild/repository/Recipes.java
+++ src/main/java/com/example/madspild/repository/Recipes.java
@@ -7,20 +7,29 @@ import java.util.List;
 
 public class Recipes {
 
-    private ArrayList<Recipe> recipes = new ArrayList<>();
+    private final ArrayList<Recipe> RECIPES = new ArrayList<>();
 
     public Recipes() {
         for (int i = 0; i < 100; i++) {
-            recipes.add(new Recipe("name" + i, "This is a description of a recipe"));
+            RECIPES.add(new Recipe("name" + i, "This is a description of a recipe"));
         }
 
-        recipes.get(0).addIngredient("butter", 400);
-        recipes.get(0).addIngredient("sugar", 400);
-        recipes.get(0).addIngredient("flour", 400);
-        recipes.get(0).addIngredient("baking powder", 2);
+        RECIPES.get(0).addIngredient("butter", 400);
+        RECIPES.get(0).addIngredient("sugar", 400);
+        RECIPES.get(0).addIngredient("flour", 400);
+        RECIPES.get(0).addIngredient("baking powder", 2);
     }
 
     public List<Recipe> getAllRecipes() {
-        return recipes;
+        return RECIPES;
+    }
+
+    public Recipe getRecipe(String recipe) {
+        for (Recipe r : RECIPES) {
+            if (r.getName().equals(recipe)) {
+                return r;
+            }
+        }
+        return null;
     }
 }
