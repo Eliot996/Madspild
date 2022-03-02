e80b3e22bfce0df0b8f32e99adc826d99ca902ad
Eliot996 <math10t6@stud.kea.dk>
2022-03-02 10:55:21 +0100

    added h3 tags

--- src/main/java/com/example/madspild/repository/Recipes.java
+++ src/main/java/com/example/madspild/repository/Recipes.java
@@ -12,15 +12,14 @@ public class Recipes {
 
     public Recipes() {
         for (int i = 0; i < 100; i++) {
-            RECIPES.add(new Recipe("name" + i, "Pandekagedej:\n" +
+            RECIPES.add(new Recipe("name" + i, "<h3>Pandekagedej:</h2>\n" +
                     "Æg og mel piskes sammen. Heri piskes det varme smeltede smør, spæd med mælk og smages til med salt, sukker og vanille.\n" +
                     "\n" +
                     "Bag pandekagerne lysebrune i smør på panden. Der bliver 8-12 pandekager alt efter pandens størrelse. 12 stk. med pande på 19 cm. i diameter.\n" +
                     "\n" +
                     "Server pandekagerne med, sukker og syltetøj, blød vanilleis eller flødeskum.\n" +
                     "\n" +
-                    "Tips:\n" +
-                    "\n" +
+                    "<h3>Tips:</h3>\n" +
                     "Det kan være en fordel at have en pande som kun bruges til pandekager, for at undgå de hænger fast på panden.\n" +
                     "Da du ikke i så høj grad har fastbrændte rester eller slidt pande efter høj varme.\n" +
                     "\t"));
